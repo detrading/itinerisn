afffbcbe50e9f3776e7fea132b37fb6142a154fc
nevn <37251801+nevn@users.noreply.github.com>
2018-03-18 10:25:40 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<?PHP
+//travel planner
+
+//population per sq km
+  //various params indicators to compare and proportion populations densities per location
+  
+//external factors - crime/safety index of traveling countries and areas within
+//economic, cultural, & demographic info - per country
+
+//various estimated determinate for valued economic, cultural, & demographic info per region
+//highlight landmarks
